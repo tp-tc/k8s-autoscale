721b13b61df4ee9bb788e154c0c7880a953e9d10
Rail Aliiev <rail@mozilla.com>
2019-11-11 18:53:26 -0500

    Revert docker changes (#61)
    * Revert "Fix scopes (#60)"
    
    This reverts commit 215f72695a5538507e913c6ec08e59530179be70.
    
    * Revert "Use official docker image (#58)"
    
    This reverts commit 78b53cdf02b28e4da1d112c3880132548355ddf0.

--- docker.d/push_image.sh
+++ docker.d/push_image.sh
@@ -1,16 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 test $SECRET_URL
 test $DOCKERHUB_EMAIL
 test $DOCKERHUB_USER
 test $TAG
 
-apk -U add jq
-
-dockerhub_password=$(wget -qO- $SECRET_URL | jq '.["secret"]["docker"]["password"]')
+dockerhub_password=$(curl $SECRET_URL | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["docker"]["password"]')
 
 docker login -e $DOCKERHUB_EMAIL -u $DOCKERHUB_USER -p $dockerhub_password
 docker push $TAG
-
-echo "=== Clean up ==="
-rm -f /root/.dockercfg
